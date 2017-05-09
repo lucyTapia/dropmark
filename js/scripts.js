988c78606515b2634ff9eb926e07b95f9b539db5
lucyTapia <mtapia2@vcd-ceb131-03.eastern.ewu.edu>
2017-05-09 16:42:37 -0700

    Done for today
    idk but the link isn’t working

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,58 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
 
-console.log('Hello, World!');
+// Configure JSON Call 
+// The API feed. ENDPOINT,
+
+var getStuff = 'http://mlucyt.dropmark.com/420952.json';
+
+// Optional settings for private collections. This is public, no settings needed. 
+var setStuff = {};
+
+// Do stuff with returned data
+var doStuff = function(data) {
+
+console.log(data.items);
+  //console.log(data.items[0].link);
+  //console.log(data.items[0].name);
+  //console.log(data.items[0].thumbnails.cropped);
+  //console.log(data.items[0].preview); 
+ 
+    
+    // What do you want to do? Show (display on page) data? 
+ showStuff(data);
+  
+};
+
+
+//Get & Set 
+
+$.getJSON( getStuff, setStuff, doStuff);
+
+// Display it 
+var showStuff = function(data) { 
+    
+    // Put on page 
+    $('h1').text(data.name);
+    $('p a').attr('href', data.url)
+
+
+    // Child Objects, May have many children.
+    // Loop or cycle through all the data
+
+    $.each(data.items, function() {
+        // loop
+
+        var stuff = '<p>' + data.items[i].name + '</p>';
+
+        //puy on page 
+        $('#items').append(stuff);
+
+
+
+    });
+
+};
+
+
+
